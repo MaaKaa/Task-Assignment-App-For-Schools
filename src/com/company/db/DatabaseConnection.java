e36c3ee20f5c953fed1284f84fa32191a105f7dc
Marzena <kacprowicz.marzena@gmail.com>
2018-12-02 14:45:31 +0100

    Initial commit

---
+++ src/com/company/db/DatabaseConnection.java
@@ -0,0 +1,10 @@
+package com.company.db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    public static Connection getConnection() throws SQLException {
+
+    }
+}
